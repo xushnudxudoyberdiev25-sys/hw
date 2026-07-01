134289bc1c382e4727823bb7aee921eea08fdfcf
xushnud <xushnudxudoyberdiev25@gmail.com>
2026-07-01 12:46:25 +0500

    1-problem

--- pr1.py
+++ pr1.py
@@ -1,1 +1,12 @@
-print("hello")
+def count_vowels_and_consonants(text: str) -> dict:
+    text = text.lower()
+    unli = 0
+    undosh = 0
+    for word in text:
+        if word == "a" or word == "e" or word == "i" or word == "o" or word == "u":
+            unli += 1
+        elif word.isalpha():
+            undosh += 1
+    return {"unli": unli, "undosh": undosh}
+
+print(count_vowels_and_consonants("Salom Dunyo!"))
